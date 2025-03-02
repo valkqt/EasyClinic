e96c8ac589d543ecdf99e631949dea9300534819
valkqt <bznand@gmail.com>
2025-03-02 16:03:52 +0100

    added ssl

--- frontend/src/resources/axios.ts
+++ frontend/src/resources/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 // defaults to remote API, change "baseURL" to the localhost URL in case the application is running locally, for example "http://localhost:7221"
 
 export const api = axios.create({
-  baseURL: "http://andreabuzzanca-001-site4.jtempurl.com/api/",
+  baseURL: "https://andreabuzzanca-001-site4.jtempurl.com/api/",
   timeout: 5000,
   headers: {},
 });
